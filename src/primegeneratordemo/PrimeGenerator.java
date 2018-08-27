b9592f38d699079ebf8a525c82735d9de3a73a75
Jordan Morris <21jordanrmorris@gmail.com>
2018-08-26 22:32:27 -0500

    Initial Commit; Switching IDEs

---
+++ src/primegeneratordemo/PrimeGenerator.java
@@ -0,0 +1,156 @@
+package primegeneratordemo;
+
+import java.util.ArrayList;
+
+/**
+ * Provides methods that perform the Sieve of Eratosthenes
+ * and brute-force algorithm or assist in doing so.
+ * CSC 3102 Programming Project # 0
+ * @author Jordan Morris
+ * @since 8-22-2018
+ * @see PrimeGeneratorAPI
+ */
+
+public class PrimeGenerator implements PrimeGeneratorAPI {
+    
+    /**
+     * A reference to a boolean array.
+     */
+    private boolean[] sequence;
+    
+    /**
+     * An integer one less than the capacity of the array to account
+     * for index 0 in the sequence array.
+     */
+    private int length;
+    
+    /**
+     * Creates a Boolean sequence of length 100 using the Sieve of
+     * Eratosthenes, where sequence[k] is true, when k is prime and
+     * false when k is composite.
+     */
+    public PrimeGenerator() {
+        sequence = new boolean[100];
+        length = sequence.length;
+        eratosthenesSieve(sequence);
+    }
+    
+    /**
+     * Generates a boolean sequence [0,...,length] that indicates
+     * whether sequence[k] is prime or composite using the
+     * specific algorithm
+     * @param n an integer >= 1
+     * @param alg 'E' or 'e' for the Sieve of Eratosthenes sieve
+     * and 'n' or 'N' for the naive/brute-force algorithm
+     * @throws IllegalArgumentException when n is less than 1 or 
+     * parameter alg is not 'n', 'N', 'E', or 'e'
+     */
+    public PrimeGenerator(int n, char alg) throws IllegalArgumentException {
+        if(n < 1)
+            throw new IllegalArgumentException("Must enter a positive non-zero integer.");
+        else if(alg != 'n' && alg != 'N' && alg != 'e' && alg != 'E')
+            throw new IllegalArgumentException("Parameter alg must be one of 'n', 'N', 'E', or 'e'");
+        
+        sequence = new boolean[n+1];
+        length = sequence.length;
+        if(alg == 'n' || alg == 'N') {
+            naiveSieve(sequence);
+        }
+        else {
+            eratosthenesSieve(sequence);
+        }
+    }
+    
+    /**
+     * An auxiliary method that sets seq[k] to true if k is prime
+     * and false if k is composite using the Eratosthenes Sieve
+     * algorithm.
+     * @param seq a boolean array that indicates whether or not
+     * k is prime
+     */
+    private void eratosthenesSieve(boolean[] seq) {
+        length = seq.length;
+        seq[0] = false;
+        seq[1] = false;
+        if(length >= 2) {
+            for(int i = 2; i < length; i++)
+                seq[i] = true;
+            for(int p = 2; p*p <= length; p++) {
+                if(seq[p] == true) {
+                    for(int i = p*2; i < length; i+= p)
+                        seq[i] = false;
+                }
+            }
+        }   
+    }
+
+    /**
+     * An auxiliary method that sets seq[k] to true if k is prime
+     * and false if k is composite using the brute-force algorithm.
+     * @param seq a boolean array that indicates whether or not
+     * k is prime
+     */
+    private void naiveSieve(boolean[] seq) {
+        length = seq.length;
+        for(int i = 0; i < length; i++) {
+            seq[i] = isPrime(i);
+        }
+    }
+    
+    public ArrayList<Integer> generate(int n) throws IllegalArgumentException {
+        if(n > length)
+            throw new IllegalArgumentException("Upper bound cannot exceed size of current sequence");
+        ArrayList<Integer> upperBoundedArray = new ArrayList<Integer>();
+        for(int i = 0; i <= n; i++) {
+            if(sequence[i] == true)
+                upperBoundedArray.add(i);
+        }
+        return upperBoundedArray;
+    }
+    
+    public boolean isPrime(int n) {
+        if(n < 2)
+            return false;
+        else if(n == 2 || n == 3 || n == 5)
+            return true;
+        else if(n%2 == 0)
+            return false;
+        else if(((n%6)-1 == 0) || ((n%6)-5 == 0)) {
+            for(int i = 3; i <= Math.sqrt(n); i+=2) {
+                if(n%i == 0)
+                    return false;
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    public int getPrime(int nth) throws IllegalArgumentException {
+        return 0;
+    }
+    
+    public int getMax() throws IllegalArgumentException {
+        for(int i = sequence.length-1; i >= 0; i--) {
+            if(sequence[i] == true)
+                return i;
+        }
+        throw new IllegalArgumentException("There are no prime numbers in the given set of numbers");
+    }
+    
+    public int size() {
+        return length;
+    }
+    
+    public String toString() {
+        String pseq = "[";
+        for(int i = 0; i < size(); i++) {
+            if(sequence[i] == true)
+                pseq += i + ", ";
+        }
+        if(pseq.length() == 1)
+            return pseq + "]";
+        return pseq.substring(0, pseq.length()-2) + "]";
+    }
+}
---
+++ src/primegeneratordemo/PrimeGeneratorDemo.java
@@ -0,0 +1,34 @@
+package primegeneratordemo;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * A test bed for the PrimeGenerator implementation.  
+ * @author Duncan, Jordan Morris
+ * @since 8-21-2018
+ * @see PrimeGeneratorAPI, PrimeGenerator.
+ */
+
+public class PrimeGeneratorDemo 
+{
+    public static void main(String[] args) 
+    {
+        
+        Scanner cin = new Scanner(System.in);
+        System.out.print("Enter a positive integer -> ");
+        int n = cin.nextInt();
+        PrimeGenerator primeSeq = new PrimeGenerator(n,'n');
+        System.out.printf("Is %d a prime number? %b%n",n,primeSeq.isPrime(n));
+        System.out.printf("Prime numbers in [1,%d] are %s.%n",n,primeSeq);
+        System.out.printf("The largest prime number in [1,%d] is %d.%n",n,primeSeq.getMax());
+        ArrayList<Integer> primes = primeSeq.generate(n);
+        System.out.printf("The number of prime numbers in [1,%d] is %d.%n",n,primes.size());        
+        //Add code to generate a random prime in [2,n] and the table shown on handout:
+
+		
+		
+    }
+    
+}
